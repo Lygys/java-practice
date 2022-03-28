bd344a5af73e6e667ecf3056eec7c781f8e38c46
小川舜人 <duc.de.alphaasile@gmail.com>
2022-03-28 10:12:13 +0900

    4章読了

---
+++ Chap4/Practice4_4.java
@@ -0,0 +1,12 @@
+public class Practice4_4 {
+    public static void main(String[] args) {
+    	int[] numbers = { 3, 4, 9 };
+    	System.out.println("1桁の数字を入力してください");
+    	int input = new java.util.Scanner(System.in) .nextInt();
+    	for (int n : numbers) {
+    		if (n == input) {
+    			System.out.println("アタリ!");
+    		}
+    	}
+    }
+}
