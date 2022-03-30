948e18d36643a697f5da9b78d2295c98cb55d674
小川舜人 <duc.de.alphaasile@gmail.com>
2022-03-30 10:54:27 +0900

    8章読了

---
+++ Chap6/Kouhan.java
@@ -0,0 +1,12 @@
+package comment;
+public class Kouhan {
+    public static void callDeae()  {
+    	System.out.println("えぇい、小癪な。曲者だ！　であえい！");
+    }
+    public static void showMondokoro() throws Exception {
+    	System.out.println("飛車さん、角さん。もういいでしょう。");
+    	System.out.println("この紋所が目に入らぬか！");
+    	Thread.sleep(3000);
+    	Zenhan.doTogame();
+    }
+}
