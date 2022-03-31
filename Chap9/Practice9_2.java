262e58faf69ec614916bb3f5e021ad040cfebe37
小川舜人 <duc.de.alphaasile@gmail.com>
2022-03-31 11:04:52 +0900

    9章読了

---
+++ Chap9/Practice9_2.java
@@ -0,0 +1,18 @@
+import Thief;
+public class Practice9_2 {
+	public static void heal(int hp) {
+		hp += 10;
+	}
+	public static void heal(Thief thief) {
+		thief.hp += 10;
+	}
+	
+    public static void main(String[] args) {
+    	int baseHp = 25;
+    	Thief t = new Thief("アサカ", baseHp);
+    	heal(baseHp);
+    	System.out.println(baseHp + " : " + t.hp);
+    	heal(t);
+    	System.out.println(baseHp + " : " + t.hp);
+    }
+}
