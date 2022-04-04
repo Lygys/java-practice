b8ef85c772671ce07926a11602a02f11a96f4daf
小川舜人 <duc.de.alphaasile@gmail.com>
2022-04-04 09:36:15 +0900

    11章読了

---
+++ Chap11/TangibleAsset.java
@@ -0,0 +1,23 @@
+package practice;
+
+public abstract class TangibleAsset extends Asset implements Thing {
+    String color;
+    double weight;
+    
+    public TangibleAsset(String name, int price, String color) {
+    	super(name, price);
+    	this.color = color;
+    }
+    
+	public String getColor() {
+		return this.color;
+	}
+
+	public double getWeight() {
+		return this.weight;
+	}
+
+	public void setWeight(double weight) {
+		this.weight = weight;
+	}
+}
