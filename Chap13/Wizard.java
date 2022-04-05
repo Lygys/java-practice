ced01e184ae9b89681528df4a03a365d8e8fb166
小川舜人 <duc.de.alphaasile@gmail.com>
2022-04-05 09:17:33 +0900

    13章読了

---
+++ Chap13/Wizard.java
@@ -0,0 +1,58 @@
+package practice;
+public class Wizard {
+	private int hp;
+	private int mp;
+	private String name;
+	private Wand wand;
+	
+	public void heal(Hero h) {
+    	int basePoint = 10;
+    	int recovPoint = (int)(basePoint * this.wand.power);
+    	h.setHp(h.getHp() + recovPoint);
+    	System.out.println(h.getName() + "のHPを" + recovPoint + "回復した！");
+    }	
+	
+    public int getHp() {
+		return hp;
+	}
+
+	public int getMp() {
+		return mp;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public Wand getWand() {
+		return wand;
+	}
+
+	public void setHp(int hp) {
+		if (hp < 0) { this.hp = 0; }
+		else { this.hp = hp; }
+	}
+
+	public void setMp(int mp) {
+		if (mp < 0) {
+			throw new IllegalArgumentException("設定されようとしているMPが異常です");
+		}
+		this.mp = mp;
+	}
+
+	public void setName(String name) {
+		if (name == null || name.length() < 3 ) {
+			throw new IllegalArgumentException("魔法使いに設定されようとしている名前が異常です");
+		}
+		this.name = name;
+	}
+
+	public void setWand(Wand wand) {
+		if (wand == null) {
+			throw new IllegalArgumentException("設定されようとしている杖がnullです");
+		}
+		this.wand = wand;
+	}
+
+
+}
