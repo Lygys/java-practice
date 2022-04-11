b1d5f3dfc307922761a526e3bef8026ed2efa5fe
小川舜人 <duc.de.alphaasile@gmail.com>
2022-04-11 16:55:03 +0900

    17章読了

--- Chap17/Practice17_1.java
+++ Chap17/Practice17_1.java
@@ -1,6 +1,13 @@
 public class Practice17_1 {
     public static void main(String[] args) {
-    	String s = null;
-    	System.out.println(s.length());
+    	try {
+	    	String s = null;
+	    	System.out.println(s.length());
+    	} catch (NullPointerException e) {
+    		System.out.println("NullPointerException例外をcatchしました");
+    		System.out.println("--スタックトレース(ここから)--");
+    		e.printStackTrace();
+    		System.out.println("--スタックトレース(ここまで)--");
+    	}
     }    
 }
